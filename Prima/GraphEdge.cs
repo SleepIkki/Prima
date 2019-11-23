aa43dea26c08ecdac87c10ce56bc41554d959924
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-23 19:47:37 +0300

    Created class to represent the edge of the graph, class to represent the spanning tree by Prim. Here are represented the main data, constructor and additional method to count the points of the graph, called GetPoints.

---
+++ Prima/GraphEdge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prima
+{
+    class GraphEdge
+    {
+        public int Point1 { get; set; } = 0;
+        public int Point2 { get; set; } = 0;
+        public int Weight { get; set; } = 0;
+
+        public GraphEdge(int p1, int p2, int weight)
+        {
+            Point1 = p1;
+            Point2 = p2;
+            Weight = weight;
+        }
+    }
+}
---
+++ Prima/PrimSpanningTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prima
+{
+    class PrimSpanningTree
+    {
+        public List<GraphEdge> NotUsedEdges { get; } = new List<GraphEdge>();
+        public List<GraphEdge> SpanningTree { get; } = new List<GraphEdge>();
+        List<int> UsedPoints = new List<int>();
+        List<int> NotUsedPoints = new List<int>();
+
+        public PrimSpanningTree(List<GraphEdge> graph)
+        {
+            NotUsedEdges = graph;
+            GetPoints();
+        }
+
+        public void Prim()
+        {
+
+        }
+
+        private void GetPoints()
+        {
+            for(int i = 0; i < NotUsedEdges.Capacity; ++i)
+            {
+                if(NotUsedPoints == null || !NotUsedPoints.Contains(NotUsedEdges[i].Point1))
+                {
+                    NotUsedPoints.Add(NotUsedEdges[i].Point1);
+                }
+                if (NotUsedPoints == null || !NotUsedPoints.Contains(NotUsedEdges[i].Point2))
+                {
+                    NotUsedPoints.Add(NotUsedEdges[i].Point2);
+                }
+            }
+        }
+    }
+}
