75a6e140d757aba4b1a5b915b68b4ca3986907ce
Semen <45079454+SleepIkki@users.noreply.github.com>
2019-11-24 14:46:25 +0300

    Prim logic is ready, not tested.

--- Prima/PrimSpanningTree.cs
+++ Prima/PrimSpanningTree.cs
@@ -19,7 +19,47 @@ namespace Prima
 
         public void Prim()
         {
+            int countPoints = NotUsedPoints.Count;
 
+            for (int i = 0; i < countPoints; ++i)
+                NotUsedPoints.Add(i);
+
+            Random rnd = new Random();
+            UsedPoints.Add(rnd.Next(0, countPoints));
+            NotUsedPoints.RemoveAt(UsedPoints[0]);
+
+            while (NotUsedPoints.Count > 0)
+            {
+                int minEdge = -1;
+                for (int i = 0; i < NotUsedEdges.Count; ++i)
+                {
+                    if ((UsedPoints.IndexOf(NotUsedEdges[i].Point1) != -1) && (NotUsedPoints.IndexOf(NotUsedEdges[i].Point2) != -1) ||
+                (UsedPoints.IndexOf(NotUsedEdges[i].Point2) != -1) && (NotUsedPoints.IndexOf(NotUsedEdges[i].Point1) != -1))
+                    {
+                        if (minEdge != -1)
+                        {
+                            if (NotUsedEdges[i].Weight < NotUsedEdges[minEdge].Weight)
+                                minEdge = i;
+                        }
+                        else
+                            minEdge = i;
+                    }
+                }
+
+                if (UsedPoints.IndexOf(NotUsedEdges[minEdge].Point1) != -1)
+                {
+                    UsedPoints.Add(NotUsedEdges[minEdge].Point2);
+                    NotUsedPoints.Remove(NotUsedEdges[minEdge].Point2);
+                }
+                else
+                {
+                    UsedPoints.Add(NotUsedEdges[minEdge].Point1);
+                    NotUsedPoints.Remove(NotUsedEdges[minEdge].Point1);
+                }
+
+                SpanningTree.Add(NotUsedEdges[minEdge]);
+                NotUsedEdges.RemoveAt(minEdge);
+            }
         }
 
         private void GetPoints()
